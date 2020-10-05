d56ac32d43ae5b3e9f2d848613f5d1f2c017b11b
Paweł Zdralewicz <pawel.zdralewicz@gmail.com>
2020-10-05 20:10:12 +0200

    Fill structural-typing

--- workshop/04 - structural-typing.ts
+++ workshop/04 - structural-typing.ts
@@ -1,10 +1,33 @@
 // The basic rule for TypeScript’s structural type system is that x is compatible with y if y has at least the same members as x.
-
-const quack = (duck: Duck) => {
-  console.log('quack quack');
+interface Duck {
+  legs: number;
+  makesSound: boolean;
 }
 
-const 
+interface Dog {
+  legs: number;
+  makesSound: boolean;
+  bark: () => void;
+}
 
-quack()
+const quack = (duck: Duck) => {
+  console.log(
+    `I have ${duck.legs} legs${duck.makesSound ? " and I make sound" : "."}`
+  );
+};
 
+const dog: Dog = {
+  legs: 4,
+  makesSound: true,
+  bark: () => console.log("Woof!"),
+};
+
+const silentDuck: Duck = {
+  legs: 2,
+  makesSound: false,
+};
+
+// works for both above objects as long as Dog interface satifies every field from Duck interface
+// it called "structural typing" (or duck-typing)
+quack(silentDuck);
+quack(dog);
